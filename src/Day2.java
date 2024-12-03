1324e6bf4ab98d93978b5cccaf485aba16b0ed67
Monika <monika.dybalska@hotmail.com>
2024-12-03 22:19:46 +0100

    clean up Day 2 solution

--- src/Day2.java
+++ src/Day2.java
@@ -8,27 +8,30 @@ public class Day2 {
         ArrayList<ArrayList<Integer>> arrs = readFile("input/day2");
 
         System.out.println(arrs.size() - redNosedReportsPart1(arrs).size());
-        System.out.println(redNosedReportsPart2(arrs));
+        System.out.println(arrs.size() - redNosedReportsPart2(arrs));
+    }
+
+    public static boolean isReportSafe(ArrayList<Integer> arr) {
+        int i = 0;
+        boolean safe = true;
+        int order = arr.get(0) < arr.get(1) ? -1 : 1;
+
+        while (safe && i < arr.size() - 1) {
+            int diff = (arr.get(i) - arr.get(i + 1)) * order;
+            if (diff < 1 || diff > 3) {
+                safe = false;
+            }
+            i++;
+        }
+
+        return safe;
     }
 
     public static ArrayList<ArrayList<Integer>> redNosedReportsPart1(ArrayList<ArrayList<Integer>> reports) {
         ArrayList<ArrayList<Integer>> unsafeReports = new ArrayList<>();
 
         for (ArrayList<Integer> report : reports) {
-            boolean safe = true;
-            int i = 0;
-            String order = getOrder(report.get(0), report.get(1));
-
-            while (safe && i < report.size() - 1) {
-                int curr = report.get(i);
-                int next = report.get(i + 1);
-                String currOrder = getOrder(curr, next);
-                int diff = Math.abs(curr - next);
-                if (!order.equals(currOrder) || diff < 1 || diff > 3) {
-                    safe = false;
-                }
-                i++;
-            }
+            boolean safe = isReportSafe(report);
 
             if (!safe) {
                 unsafeReports.add(report);
@@ -39,73 +42,24 @@ public class Day2 {
     }
 
     public static int redNosedReportsPart2(ArrayList<ArrayList<Integer>> reports) {
-        int safeReportsCount = 0;
-
-        for (ArrayList<Integer> report : reports) {
-            int unsafeLevelsCount = 0;
-            int i = 1;
-            int curr = report.getFirst();
-            int next = report.get(i);
-            String order = getOrder(curr, next);
-
-            while (unsafeLevelsCount <= 1 && i < report.size()) {
-                next = report.get(i);
-                int diff = Math.abs(curr - next);
-                String currOrder = getOrder(curr, next);
-                if (!order.equals(currOrder) || diff < 1 || diff > 3) {
-                    if (i > 1) {
-                        i++;
-                    } else {
-                        curr = next;
-                        currOrder = getOrder(curr, report.get(i+1));
-                    }
-                    unsafeLevelsCount++;
-                } else {
-                    curr = next;
-                }
-                order = currOrder;
-                i++;
-            }
-
-            if (unsafeLevelsCount <= 1) {
-                safeReportsCount++;
-            }
-        }
-
-        return safeReportsCount;
-    }
-
-    public static int redNosedReportsPart2V2(ArrayList<ArrayList<Integer>> reports) {
         ArrayList<ArrayList<Integer>> unsafeReports = redNosedReportsPart1(reports);
-        int unsafeReportsWithDampener = unsafeReports.size();
+        int unsafeReportsCount = unsafeReports.size();
 
         for (ArrayList<Integer> report : unsafeReports) {
             boolean safe = false;
-
-            for (int levelId = 0; levelId < report.size(); levelId++) {
-                safe = true;
+            int i = 0;
+            while (!safe && i < report.size()) {
                 ArrayList<Integer> arrCopy = new ArrayList<>(report);
-                arrCopy.remove(levelId);
-                int i = 0;
-                String order = getOrder(arrCopy.get(0), arrCopy.get(1));
-                while (safe && i < arrCopy.size() - 1) {
-                    int curr = arrCopy.get(i);
-                    int next = arrCopy.get(i + 1);
-                    String currOrder = getOrder(curr, next);
-                    int diff = Math.abs(curr - next);
-                    if (!order.equals(currOrder) || diff < 1 || diff > 3) {
-                        safe = false;
-                    }
-                    i++;
-                }
-                if (safe) {
-                    unsafeReportsWithDampener--;
-                    break;
-                }
+                arrCopy.remove(i);
+                safe = isReportSafe(arrCopy);
+                i++;
+            }
+            if (safe) {
+                unsafeReportsCount--;
             }
         }
 
-        return unsafeReportsWithDampener;
+        return unsafeReportsCount;
     }
 
     public static ArrayList<ArrayList<Integer>> readFile(String fileName) throws FileNotFoundException {
@@ -124,15 +78,5 @@ public class Day2 {
         }
 
         return reports;
-    }
-
-    public static String getOrder(int curr, int next) {
-        if (curr > next) {
-            return "desc";
-        } else if (curr < next) {
-            return "asc";
-        } else {
-            return "equal";
-        }
     }
 }
